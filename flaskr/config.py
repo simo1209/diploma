cf121dc126cdb2b471b7b3ad793b632bea3d2083
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-12-24 09:20:33 +0200

    Added configuration file

---
+++ flaskr/config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig(object):
+    SECRET_KEY = 'secret'
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 10
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/qrpayment'
