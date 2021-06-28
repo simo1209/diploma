bcd7c9ebf753f839028bd458dc300ed3280f5c7f
simo1209 <simeongeorgiev09@gmail.com>
2021-06-28 16:41:25 +0300

    Fixed merge conflict in config.py

--- flask/diplomaproject/config.py
+++ flask/diplomaproject/config.py
@@ -18,14 +18,8 @@ class BaseConfig(object):
     BCRYPT_LOG_ROUNDS = 10
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
-<<<<<<< HEAD:flask/diplomaproject/config.py
-    QR_CODES = '/home/simo/qr-codes'
-    FLASK_ADMIN_SWATCH = 'cerulean'
-
-=======
     QR_CODES = '/home/simo09/qr-codes'
     FLASK_ADMIN_SWATCH = 'cerulean'
->>>>>>> 9b813e16178f8918f0a2377e583c0df6a84532e1:diplomaproject/config.py
 
 
 class DevelopmentConfig(BaseConfig):
